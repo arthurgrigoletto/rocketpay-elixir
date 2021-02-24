2e53fc43a16c4ec0127c6b2198a0d8b7ad47f7ce
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2021-02-24 11:30:57 -0300

    Finish Third class

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,5 +1,9 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
 
+  alias Rocketpay.Accounts.Deposit
+
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(paras), to: Deposit, as: :call
 end
